54e360c4b82dde772fee10e2321414cd706bb2c1
RNFcraft <rnfcraft77@gmail.com>
2026-09-22 19:59:30 +0300

    День 3. Выполнил задание с калькулятором.

---
+++ calc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void) {
+    double a, b, res;
+    char oper;
+
+    printf("Введите пример в формате a + b: \n");
+
+    scanf("%lf %c %lf", &a, &oper, &b);
+
+    switch (oper)
+    {
+    case '+':
+        res = a + b;
+        break;
+        
+    case '-':
+        res = a - b;
+        break;
+        
+    case '*':
+        res = a * b;
+        break;
+    
+    case '/':
+        if(b != 0){
+            res = a / b;
+        } else {
+        printf("Деление на ноль невозможно.\n");
+        return 1;
+        } 
+    break;
+
+    }
+    printf("%lf\n", res);
+    
+    return 0;
+}
